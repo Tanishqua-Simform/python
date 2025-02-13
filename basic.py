6ca2c217b531ba9989d3bb333d8f8937c4f369ab
Tanishqua-Simform <tanishqua.bansal@simformsolutions.com>
2025-02-13 19:48:59 +0530

    Experimenting with datatypes

--- basic.py
+++ basic.py
@@ -53,7 +53,7 @@ def memoryFunctioning():
     print('a[2] is b[2] - ', a[2] is b[2])
     print('\nThis is because List/Tuple only contains the memory location of items and not the items themselves.')
 
-memoryFunctioning()
+# memoryFunctioning()
 
 def dataTypes():
     print('\nModifying list inside tuple example - ')
@@ -64,14 +64,104 @@ def dataTypes():
     names[4].insert(2, [12, 13, 14, 15, 16])
     print(names)
 
-    print('Modifying tuple list inside list example - ')
+    print('\nModifying tuple list inside list example - ')
     cars = ['Ferrari', 'Merc', 'BMW', ('Lamborgini', 'McLaren')]
     print(cars)
     cars[3] = 'laz'
     print(cars)
 
-    print('Boolean')
+    print('\nBoolean')
     print('1' == True, '0' == False, 1 == True, 0 == False)
     print('None - ', bool(None))
 
-dataTypes()
+    print('\nTuples')
+    tup0 = (50,) # tup0 = (50) will give error in for loop 
+    print(type(tup0))
+    tup1 = (50)
+    print(type(tup1))
+    tup2 = ('a', 'b', 'c')
+    tup2 = (1, 2, 3) # Possible to reassign value but not change any item inside tuple.
+    print(tup2)
+
+    print('\nUnpacking in tuple')
+    tup3 = (1, 2, 3, 4, 5, 6)
+    x, y, *z = tup3
+    print('x, y, *z = ', x, y, z)
+    x, *y, z = tup3
+    print('x, *y, z = ', x, y, z)
+    *x, y, z = tup3
+    print('*x, y, z = ', x, y, z)
+
+    print('\nList')
+    print(id([1,2,3]))
+    l1 = [1,2,3]
+    print(id(l1))
+    print(id([1,2,3]))
+    l2 = [1,2,3]
+    print(id(l2))
+    print(id([1,2,3]))
+
+    print('\nList copy with slice')
+    l3 = l2[1:]
+    print('l3 is l2', l3 is l2)
+    print('l3[0] is l2[1]', l3[0] is l2[1])
+
+# dataTypes()
+
+def memoryInFunctions():
+
+    print('\nFor immutable datatypes like numeric\n')
+    def testfunction(arg):
+        print ("ID inside the function:", id(arg))
+        arg = arg + 1
+        print ("new object after increment", arg, id(arg))
+
+    var=10
+    print ("ID before passing:", id(var))
+    testfunction(var)
+    print ("value after function call", var)
+
+    print('\nFor mutable datatypes like list, dict\n')
+    def testfunction(arg):
+        print ("Inside function:",arg)
+        print ("ID inside the function:", id(arg))
+        arg=arg.append(100)
+    
+    var=[10, 20, 30, 40]
+    print ("ID before passing:", id(var))
+    testfunction(var)
+    print ("list after function call", var)
+
+# memoryInFunctions()
+
+def boolInt():
+    a = [True, 0, 1, False, True, 2]
+    b = 0
+    c = 0
+    for i in a:
+        if isinstance(i, bool):
+            b += 1
+        elif isinstance(i, int):
+            c += 1
+
+    print(b, c)
+    
+    print(id(1))
+    print(id(bool(1)))
+    num = 1
+    numBool = bool(1)
+    print(id(num))
+    print(id(numBool))
+
+
+    print(isinstance(1.0, float))
+    print(isinstance(1, float))
+    print(1 == 1.0)
+
+    print(isinstance(True, int))
+    print(type(isinstance(1, int)))
+
+    print(issubclass(bool, int))
+    print(issubclass(int, bool))
+
+boolInt()
